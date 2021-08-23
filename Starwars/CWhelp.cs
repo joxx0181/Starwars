ccb12df5fb3a4ab97e304ca1bf93234a67a58e1d
Jo-Anna Wahlstrøm Alfheim Sillemann <joxx0181@zbc.dk>
2021-08-23 18:18:58 +0200

    Solution

---
+++ Starwars/CWhelp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starwars
+{
+    public  static class CWhelp
+    {
+        // Method!
+        public static void sendToConsole(this IEnumerable<Planet> assignmentList)
+        {
+            // Using foreach loop to query execution!
+            foreach (Planet assignment in assignmentList)
+            {
+                Console.WriteLine(assignment.Name);
+            }
+
+            Console.WriteLine("======================");
+        }
+    }
+}
